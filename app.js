8189be7f470dc717effe4994034c9dc303ef1d4b
kirstytheproud <k.joproud@gmail.com>
2022-02-22 15:26:08 +0000

    started generating obstacles

--- app.js
+++ app.js
@@ -1,41 +1,40 @@
-//  
-    
-    const bird = document.querySelector(".bird")
-    const gameDisplay = document.querySelector('game-container')
-    const ground = document.querySelector('ground');
+//
 
-    let birdLeft = 220;
-    let birdBottom = 100;
-    let gravity = 2;
+const bird = document.querySelector(".bird");
+const gameDisplay = document.querySelector(".game-container");
+const ground = document.querySelector(".ground");
 
+let birdLeft = 220;
+let birdBottom = 100;
+let gravity = 2;
 
-   //positioning the bird in the right place
-    function startGame () {
-   
-        birdBottom -= gravity;
-        bird.style.bottom = birdBottom + 'px';
-        bird.style.left = birdLeft + 'px';
-    }
-
-   let timerId = setInterval(startGame, 20);
-
-
-    // using keypress event to make bird jump
-   function jump () {
-       birdBottom += 50
-       bird.style.bottom = birdBottom + 'px';
-       console.log(birdBottom);
-   }
-    
-
-   document.addEventListener('keyup', jump)
-        
-
-    // need to make obstacles and make them move
-function createObstacles() {
-    const obstacle = document.createElement('div')
-    obstacle.classList.add('obstacle')
-    gameDisplay.appendChild(obstacle)
+//positioning the bird in the right place
+function startGame() {
+  birdBottom -= gravity;
+  bird.style.bottom = birdBottom + "px";
+  bird.style.left = birdLeft + "px";
 }
 
-createObstacles();       
+let timerId = setInterval(startGame, 20);
+
+// using keypress event to make bird jump
+function jump() {
+  birdBottom += 50;
+  bird.style.bottom = birdBottom + "px";
+  console.log(birdBottom);
+}
+
+document.addEventListener("keyup", jump);
+
+// need to make obstacles and make them move
+function createObstacles() {
+    let obstacleLeft = 500;
+    let obstacleBottom = 150;
+  const obstacle = document.createElement("div");
+  obstacle.classList.add("obstacle");
+  gameDisplay.appendChild(obstacle);
+  obstacle.style.left = obstacleLeft + 'px'
+    obstacle.style.bottom = obstacleBottom + 'px'
+}
+
+createObstacles();
