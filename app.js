3a9a813d442a03238224e135fa82eb066a152232
kirstytheproud <k.joproud@gmail.com>
2022-02-28 11:53:42 +0000

    added gameover title

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+document.addEventListener('DOMContentLoaded' , () => {
+
 const startButton = document.querySelector(".start-btn");
 const bird = document.querySelector(".bird");
 const gameDisplay = document.querySelector(".game-container");
 const ground = document.querySelector(".ground");
 const scoreDisplay = document.getElementById("scoring");
 const timerDisplay = document.getElementById("timer");
-const gameOverText = document.querySelector(".game-over");
 
 let birdLeft = 220;
 let birdBottom = 200;
@@ -14,15 +15,12 @@ let gameIsOver = false;
 let score = 0;
 let gap = 500;
 
+//Using a start button to start the game
+const startGame = () => {
+  gameStart = true;
+};
 
-  //Using a start button to start the game
-  const startGame = () => {
-    gameStart = true;
-  }
-  
-  startButton.addEventListener("click", startGame);
-  
-
+startButton.addEventListener("click", startGame);
 
 //positioning the bird in the right place
 const playGame = () => {
@@ -53,33 +51,33 @@ const createObstacles = () => {
   let obstacleLeft = 500;
   let randomHeight = Math.random() * 100;
   let obstacleBottom = randomHeight;
-  
+
   const obstacle = document.createElement("div");
   const obstacleImg = document.createElement("img");
+  const topObstacle = document.createElement("div");
   const obstacleImgTop = document.createElement("img");
-  obstacle.classList.add("obstacle");
-  gameDisplay.appendChild(obstacle);
-
   obstacleImg.src = "8bit pipes.png";
   obstacleImgTop.src = "8bit pipes.png";
- 
+
+  obstacle.classList.add("obstacle");
+  gameDisplay.appendChild(obstacle);
   obstacle.appendChild(obstacleImg);
   obstacle.style.left = obstacleLeft + "px";
   obstacle.style.bottom = obstacleBottom + "px";
 
-  const topObstacle = document.createElement("div");
+  
   topObstacle.classList.add("topObstacle");
   gameDisplay.appendChild(topObstacle);
   topObstacle.appendChild(obstacleImgTop);
   topObstacle.style.left = obstacleLeft + "px";
-  topObstacle.style.bottom = obstacleBottom + gap+ "px";
+  topObstacle.style.bottom = obstacleBottom + gap + "px";
 
   //need to make obstacles them move towards bird
   const moveObstacle = () => {
     if (gameStart == true) {
       obstacleLeft -= 3;
       obstacle.style.left = obstacleLeft + "px";
-      topObstacle.style.left = obstacleLeft + "px"
+      topObstacle.style.left = obstacleLeft + "px";
       bird.style.bottom = birdBottom + "px";
 
       //obstacles need to disappear off the screen
@@ -87,32 +85,33 @@ const createObstacles = () => {
         clearInterval(timerId);
         gameDisplay.removeChild(obstacle);
         gameDisplay.removeChild(topObstacle);
-
       }
 
-      if (birdBottom === 0){
+      if (birdBottom === 0) {
         gameOver();
         gameIsOver = true;
       }
 
       //need to make it game over if bird collides with obstacle
-       if (obstacleLeft > 200 && obstacleLeft < 280 && birdLeft === 220 &&
-        (birdBottom < obstacleBottom + 50 || birdBottom > obstacleBottom + gap -200)
-       )
-        {
+      if (
+        obstacleLeft > 200 &&
+        obstacleLeft < 280 &&
+        birdLeft === 220 &&
+        (birdBottom < obstacleBottom + 50 ||
+          birdBottom > obstacleBottom + gap - 200)
+      ) {
         gameOver();
-        // gameIsOver = true;
       }
     }
   };
 
 
-  //Need a scoring system that addes a point every time the bird jumps over an obstacle#
+  //Scoring system that addes a point every time the bird jumps over an obstacle#
 
   const keepScore = () => {
-    if (obstacleLeft > birdLeft) {
+    if (obstacleLeft < birdLeft) {
       let scoring = score++;
-      
+
       scoreDisplay.innerHTML = `${scoring}`;
     }
     console.log(score);
@@ -121,16 +120,10 @@ const createObstacles = () => {
   keepScore();
 
   let timerId = setInterval(moveObstacle, 20);
+  if (!gameIsOver) setTimeout(createObstacles, 3000);
 
-  if(!gameIsOver) setTimeout(createObstacles, 3000);
-  
-  // if ((gameIsOver = true)) {
-  //   setTimeout(createObstacles, 3000);
-  // }
 };
 createObstacles();
-
-  
 
 //Game over screen that shows final score
 const gameOver = () => {
@@ -138,5 +131,14 @@ const gameOver = () => {
   console.log("game over");
   gameIsOver = true;
   //how you remove events
-  document.removeEventListener("keyup", control);
+  document.removeEventListener("keyup", jump);
+
+  let gameOverText = 200;
+  const gameOverTitle = document.createElement("img");
+  gameOverTitle.classList.add("game-over");
+  gameOverTitle.src = "Game-Over.png";
+  gameDisplay.appendChild(gameOverTitle);
+  gameOverTitle.style.bottom = gameOverText + "px";
 };
+
+});
